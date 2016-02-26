324f89653264ee87b384c93f2bf16172b4c7b354
jgostylo <gostyloj@yahoo.com>
2016-02-26 16:38:16 -0600

    updating the read time widget

--- app/reading-entry/index.js
+++ app/reading-entry/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('./reading-entry.css');
+
 module.exports = require('angular').module('bsr-reading-entry', [])
     .component('readingEntry', {
         template: require('./reading-entry.html'),
