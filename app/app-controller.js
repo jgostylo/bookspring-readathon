626ce360822083835343a4d3b3146edb355bae8b
Prabhakaran Thatchinamoorthy <thatchinamoorthyp@dnb.com>
2016-02-26 17:57:28 -0600

    Added the login and registration code

--- app/app-controller.js
+++ app/app-controller.js
@@ -1,20 +1,40 @@
 'use strict';
 
 /* @ngInject */
-function AppController($scope, $uibModal) {
+function AppController($scope) {
+	$scope.user = null;
+	$scope.firebaseRef = new Firebase('https://popping-heat-442.firebaseio.com');
+	
+	$scope.login = function(user) {
 
-    function showLoginModal() {
-        var modalInstance = $uibModal.open({
-            template: require('./login-modal/login-modal.html'),
-            controller: 'loginModalController'
-        });
+		$scope.firebaseRef.authWithPassword({
+			email : $scope.user.email,
+			password : $scope.user.zipCode
+		}, function(error, authData) {
+			if (error) {
+				console.log("Authentication Failed.");
+			} else {
+				console.log("Authenticated successfully with payload:",
+						authData.uid);
+			}
+		});
+	}
+	
+	$scope.createUser = function(user) {
+		var firebaseURL = 'https://popping-heat-442.firebaseio.com'
+		var firebase = new Firebase(firebaseURL);
 
-        modalInstance.result.then(function () {
-            console.log('done');
-        });
-    }
-
-    showLoginModal();
+		$scope.firebaseRef.createUser({
+			  email    : $scope.user.baseEmail,
+			  password : $scope.user.baseZipCode
+			}, function(error, userData) {
+			  if (error) {
+			    console.log("Error creating user:", error);
+			  } else {
+			    console.log($scope.user.baseEmail , " Successfully created user account with uid:", userData.uid);
+			  }
+			});
+	}
 
 }
 
